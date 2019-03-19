1a322173178f9ef82b7595a20d5d476d3ea1886c
unknown <a01054394111@gmail.com>
2019-03-19 21:07:06 +0900

    feat : input view

---
+++ src/main/java/lotto/InputView.java
@@ -0,0 +1,22 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class InputView {
+    private final static int LOTTO_LENGTH = 6;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int inputMoney(){
+        return scanner.nextInt();
+    }
+
+    public static List<Integer> inputWinnerNumber(){
+        List<Integer> winnerNumber = new ArrayList<Integer>();
+        for(int i = 0 ; i <LOTTO_LENGTH ; i++){
+            winnerNumber.add(scanner.nextInt());
+        }
+        return winnerNumber;
+    }
+}
