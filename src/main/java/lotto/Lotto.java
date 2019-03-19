712cb24511051922f4907587682ceb4577308c12
unknown <a01054394111@gmail.com>
2019-03-19 14:32:10 +0900

    feat : calculate money in Lotto.java

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -4,25 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
-    private int[] PrizeMoney = new int[]{0,0,0,5000,50000,1500000,2000000000};
+    private final static int[] PRIZE_MONEY = new int[]{0, 0, 0, 5000, 50000, 1500000, 2000000000};
+    private List<Long> countedTickets;
     private List<Integer>[] tickets;
+
+    public void setTickets(int numberOfTicket) {
+        tickets = (ArrayList<Integer>[]) new ArrayList[numberOfTicket];
+        for (int times = 0; times < numberOfTicket; times++) {
+            tickets[times] = new ArrayList<>();
+        }
+    }
+
+    public List<Long> count(List<Integer> winnerNumber) {
+        List<Long> countedTickets = new ArrayList<>();
+        for (List ticket : tickets) {
+            countedTickets.add(countSelectedTicket(ticket,winnerNumber));
+        }
+        return countedTickets;
+    }
+
     public boolean isExist(int number, List<Integer> winnerNumber) {
         return winnerNumber.contains(number);
     }
 
-    public long count(List<Integer> ticket, List<Integer> winnerNumber) {
-        return ticket.stream().filter(n-> isExist(n,winnerNumber)).count();
+    public long countSelectedTicket(List<Integer> ticket, List<Integer> winnerNumber) {
+        return ticket.stream().filter(n -> isExist(n, winnerNumber)).count();
     }
 
-    public Object seekIncome(List<Integer> countedTicket) {
+    public Object seekIncome(List<Long> countedTicket) {
         int total = 0;
-        for(int i = 0 ; i < 7 ; i++){
-            total = countedTicket.get(i)*PrizeMoney[i];
+        for (int index = 0; index < countedTicket.size(); index++) {
+            total += PRIZE_MONEY[Math.toIntExact(countedTicket.get(index))];
         }
         return total;
     }
 
-    public Object seekEarningsRate(int income, int outcome) {
+    public Object seekEarningsRate(int income) {
+        int outcome = tickets.length * 1000
         return income / outcome * 100;
     }
 
