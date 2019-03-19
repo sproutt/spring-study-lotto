2c0f393054d6ccafe8c80fcef7c3945647c8b2d5
unknown <a01054394111@gmail.com>
2019-03-19 22:54:59 +0900

    refactor : I/O

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -9,10 +9,12 @@ public class InputView {
     private static Scanner scanner = new Scanner(System.in);
 
     public static int inputMoney() {
+        System.out.println("구입 금액을 입력해주세요.");
         return scanner.nextInt();
     }
 
     public static List<Integer> inputWinnerNumber() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         List<Integer> winnerNumber = new ArrayList<Integer>();
         for (int i = 0; i < LOTTO_LENGTH; i++) {
             winnerNumber.add(scanner.nextInt());
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -3,13 +3,15 @@ package lotto.view;
 import lotto.domain.Lotto;
 import lotto.utils.Utils;
 
+import java.util.Collections;
 import java.util.List;
 
 public class OutputView {
     private static Lotto lotto = new Lotto();
 
-    public static void printEveryTickets(int numberOftickets) {
-        for (int index = 0; index < numberOftickets; index++) {
+    public static void printEveryTickets(int numberOfTickets) {
+        System.out.printf("%d개를 구매했습니다.\n",numberOfTickets);
+        for (int index = 0; index < numberOfTickets; index++) {
             List<Integer> generatedNumber = Utils.generateRandomNumbers();
             lotto.setTickets(index, generatedNumber);
             printTicket(generatedNumber);
@@ -17,6 +19,7 @@ public class OutputView {
     }
 
     public static void printTicket(List<Integer> generatedNumber) {
+        Collections.sort(generatedNumber);
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("[");
         for (int number : generatedNumber) {
@@ -24,13 +27,14 @@ public class OutputView {
         }
         stringBuilder.deleteCharAt(stringBuilder.length() - 1).append("]");
         System.out.println(stringBuilder.toString());
-        stringBuilder.setLength(0);
     }
 
     public static void printResult(List<Integer> winnerNumber) {
         List<Integer> countedNumber = lotto.count(winnerNumber);
         double EarningsRate = lotto.seekEarningsRate(
                 lotto.seekIncome(countedNumber));
+        System.out.println("당첨 통계");
+        System.out.println("---------");
         System.out.println("3개 일치 (5000원)- " + countedNumber.stream().filter(n -> n == 3).count());
         System.out.println("4개 일치 (50000원)- " + countedNumber.stream().filter(n -> n == 4).count());
         System.out.println("5개 일치 (1500000원)- " + countedNumber.stream().filter(n -> n == 5).count());
