275528cc428d8443c872f5129c1036f45cdee590
unknown <a01054394111@gmail.com>
2019-03-19 13:35:11 +0900

    add : Lotto.java

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,4 +1,20 @@
 package lotto;
 
+import java.util.List;
+
 public class Lotto {
+    public Object isExist(int i, List<Integer> list) {
+    }
+
+    public Object count(List<Integer> list, List<Integer> list2) {
+    }
+
+    public Object seekIncome(List<Integer> list2) {
+    }
+
+    public Object seekEarningsRate(int income, int outcome) {
+    }
+
+    public Object purchase(int i) {
+    }
 }
