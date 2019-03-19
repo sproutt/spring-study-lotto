a2e60620b99566a9215ca2d43a60478a550f0ff5
unknown <a01054394111@gmail.com>
2019-03-19 13:56:01 +0900

    feat : test available function in Lotto.java

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,32 @@
 package lotto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
-    public Object isExist(int i, List<Integer> list) {
+    private int[] PrizeMoney = new int[]{0,0,0,5000,50000,1500000,2000000000};
+    private List<Integer>[] tickets;
+    public boolean isExist(int number, List<Integer> winnerNumber) {
+        return winnerNumber.contains(number);
     }
 
-    public Object count(List<Integer> list, List<Integer> list2) {
+    public long count(List<Integer> ticket, List<Integer> winnerNumber) {
+        return ticket.stream().filter(n-> isExist(n,winnerNumber)).count();
     }
 
-    public Object seekIncome(List<Integer> list2) {
+    public Object seekIncome(List<Integer> countedTicket) {
+        int total = 0;
+        for(int i = 0 ; i < 7 ; i++){
+            total = countedTicket.get(i)*PrizeMoney[i];
+        }
+        return total;
     }
 
     public Object seekEarningsRate(int income, int outcome) {
+        return income / outcome * 100;
     }
 
-    public Object purchase(int i) {
+    public Object purchase(int totalPrice) {
+        return totalPrice / 1000;
     }
 }
