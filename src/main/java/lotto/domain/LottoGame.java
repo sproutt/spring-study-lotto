4b6b7212f429fddc3b4934a97f0aacbed8177a06
GiPyoo <a01054394111@gmail.com>
2019-04-03 01:07:54 +0900

    fix : LottoGame & LottoGameTest :: winningLotto를 활용하여 등수 저장기능 등을 구현

--- src/main/java/lotto/domain/LottoGame.java
+++ src/main/java/lotto/domain/LottoGame.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import lotto.utils.Sorter;
 import lotto.utils.Splitter;
 
 import java.util.ArrayList;
@@ -7,73 +8,106 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoGame {
-    private List<Lotto> lottos = new ArrayList<Lotto>();
+    private List<Lotto> lottos;
+    private List<WinningLotto> winningLottos;
+
+    public LottoGame() {
+        lottos = new ArrayList<Lotto>();
+        winningLottos = new ArrayList<WinningLotto>();
+    }
 
     public List<Lotto> getLottos() {
         return lottos;
     }
 
-    public void setLottos(List<Lotto> lottos) {
-        this.lottos = lottos;
+    public List<WinningLotto> getWinningLottos() {
+        return winningLottos;
     }
 
-    public List<Lotto> purchase(int money) {
-        for (int numbers = 0; numbers < changeUnit(money); numbers++) {
-            List<Integer> randomNumber = generateRandomNumbers();
-            Collections.sort(randomNumber);
-            Lotto lotto = new Lotto(randomNumber);
+    public void purchaseMenual(String[] texts) {
+        for (String text : texts) {
+            List<LottoNo> contrivedNumber = Splitter.splitNumber(text);
+            Lotto lotto = new Lotto(Sorter.sort(contrivedNumber));
+            lottos.add(lotto);
+        }
+    }
+
+    public List<Lotto> purchaseAuto(int money, int numberOfManual) {
+        for (int numbers = 0; numbers < numberOfAutoLotto(money, numberOfManual); numbers++) {
+            List<LottoNo> randomNumber = generateRandomNumbers();
+            Lotto lotto = new Lotto(Sorter.sort(randomNumber));
             lottos.add(lotto);
         }
         return lottos;
     }
 
-    public static List<Integer> generateRandomNumbers() {
-        List<Integer> randomNumbers = new ArrayList<Integer>();
+    public static List<LottoNo> generateRandomNumbers() {
+        List<LottoNo> randomNumbers = new ArrayList<LottoNo>();
         for (int number = 1; number <= 45; number++) {
-            randomNumbers.add(number);
+            randomNumbers.add(new LottoNo(number));
         }
         Collections.shuffle(randomNumbers);
         return randomNumbers.subList(0, 6);
     }
 
-    public int[] saveLottoResult(String text) {
-        List<Integer> winnerNumber = Splitter.splitNumber(text);
-        int[] result = new int[Rank.values().length];
-        for (int index = 0; index < lottos.size(); index++) {
-            result[countMatch(winnerNumber, index)]++;
+    public void setWinningLottos(String winningNumbertext) {
+        List<LottoNo> lottoNos = Splitter.splitNumber(winningNumbertext);
+        winningLottos = new ArrayList<>();
+        for (Lotto lotto : lottos) {
+            setWinningLotto(lotto, countMatch(lotto, lottoNos));
         }
-        return result;
     }
 
-    public int countMatch(List<Integer> winnerNumber, int index) {
+    public void setWinningLotto(Lotto lotto, int count) {
+        if (Rank.valueOf(count) != Rank.MISS) {
+            winningLottos.add(new WinningLotto(lotto, Rank.valueOf(count)));
+        }
+    }
+
+    public int countMatch(Lotto lotto, List<LottoNo> winningNumber) {
         int count = 0;
-        for (int number : winnerNumber) {
-            count = increaseCount(number, index, count);
+        for (LottoNo lottoNo : winningNumber) {
+            count = increaseCount(lotto, lottoNo, count);
         }
         return count;
     }
 
-    public int increaseCount(int number, int index, int count) {
-        if (lottos.get(index).isContain(number)) {
+    public int increaseCount(Lotto lotto, LottoNo lottoNo, int count) {
+        if (lotto.isContain(lottoNo)) {
             count++;
         }
         return count;
     }
 
-    public double calculateRate(int[] result, int outcome) {
-        int income = calculateReward(result);
-        return Math.round((double)income / outcome * 1000.0) / 10.0 ;
+    public void correctBonus(int bonusNumber) {
+        winningLottos.stream()
+                .filter(winningLotto ->
+                        winningLotto.getReward() == Rank.SECOND.getWinningMoney())
+                .forEach(winningLotto ->
+                        winningLotto.setRank(Rank.setSecond(winningLotto.isContain(new LottoNo(bonusNumber)))));
     }
 
-    public int calculateReward(int[] result) {
-        int total = 0;
-        for (int index = 0; index < result.length; index++) {
-            total += result[index] * Rank.values()[index].getReward();
+    public int countSameRank(Rank rank) {
+        return (int) winningLottos.stream().filter(winningLotto -> winningLotto.isSameRank(rank)).count();
+    }
+
+    public double calculateRate(int outcome) {
+        return (double) calculatIncome() / outcome * 100.0;
+    }
+
+    public int calculatIncome() {
+        int income = 0;
+        for (WinningLotto winningLotto : winningLottos) {
+            income += winningLotto.getReward();
         }
-        return total;
+        return income;
     }
 
     public int changeUnit(int totalPrice) {
         return totalPrice / 1000;
+    }
+
+    public int numberOfAutoLotto(int money, int numberOfManual) {
+        return changeUnit(money) - numberOfManual;
     }
 }
--- src/test/java/lotto/LottoGameTest.java
+++ src/test/java/lotto/LottoGameTest.java
@@ -5,6 +5,8 @@ import static org.hamcrest.core.Is.*;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoGame;
+import lotto.domain.LottoNo;
+import lotto.domain.Rank;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,18 +16,22 @@ import java.util.List;
 
 public class LottoGameTest {
     LottoGame lottoGame;
-    Integer[] array;
-    List<Integer> list;
+    int[] numbers;
+    LottoNo[] lottoNos;
     Lotto lotto;
     List<Lotto> lottos;
 
     @Before
     public void setUp() {
-        lottoGame = new LottoGame();
-        array = new Integer[]{1, 2, 3, 4, 5, 6};
-        list = Arrays.asList(array);
-        lotto = new Lotto(list);
         lottos = new ArrayList<>();
+        numbers = new int[]{1, 2, 3, 4, 5, 6};
+        int index = 0;
+        lottoNos = new LottoNo[6];
+        for (int number : numbers) {
+            lottoNos[index++] = new LottoNo(number);
+        }
+        lotto = new Lotto(Arrays.asList(lottoNos));
+        lottoGame = new LottoGame();
     }
 
     @Test
@@ -34,46 +40,61 @@ public class LottoGameTest {
     }
 
     @Test
-    public void 표구매() {
-        lottoGame.purchase(14000);
+    public void 수동구매() {
+        String[] text = new String[]{"1,2,3,4,5,6"};
+        lottoGame.purchaseMenual(text);
+        lottos.add(lotto);
+        assertThat(lottoGame.getLottos().size(), is(1));
+        assertThat(lottoGame.getLottos().get(0).toString(), is("[1, 2, 3, 4, 5, 6]"));
+    }
+
+    @Test
+    public void 자동구매() {
+        lottoGame.purchaseAuto(14000,0);
         List lotto = lottoGame.getLottos();
         assertThat(lotto.size(), is(14));
     }
 
     @Test
+    public void 번호_생성기() {
+        assertThat(lottoGame.generateRandomNumbers().size(), is(6));
+    }
+
+    @Test
     public void 증가() {
-        int count = 0;
-        lottos.add(lotto);
-        lottoGame.setLottos(lottos);
-        assertThat(lottoGame.increaseCount(3, 0, count), is(1));
+        LottoNo number = new LottoNo(1);
+        assertThat(lottoGame.increaseCount(lotto, number, 1), is(2));
     }
 
     @Test
     public void 몇개나_맞았나() {
-        lottos.add(lotto);
-        lottoGame.setLottos(lottos);
-        assertThat(lottoGame.countMatch(list, 0), is(6));
+        List<LottoNo> winningLotto = new ArrayList<>();
+        winningLotto = Arrays.asList(lottoNos);
+        assertThat(lottoGame.countMatch(lotto, winningLotto), is(6));
     }
 
     @Test
-    public void 결과_배열() {
-        lottos.add(lotto);
-        lottoGame.setLottos(lottos);
-        int arr[] = new int[]{0, 0, 0, 0, 0, 0, 1};
-        assertArrayEquals(lottoGame.saveLottoResult("1,2,3,4,5,6"), arr);
+    public void 보너스설정() {
+        lottoGame.setWinningLotto(lotto, 5);
+        lottoGame.correctBonus(1);
+        assertThat(lottoGame.getWinningLottos().get(0).getReward(), is(30000000));
+    }
+
+    @Test
+    public void 몇등이몇개() {
+        lottoGame.setWinningLotto(lotto, 5);
+        assertThat(lottoGame.countSameRank(Rank.SECOND), is(1));
     }
 
     @Test
     public void 비율계산() {
-        int[] result = new int[]{0, 0, 0, 1, 0, 0, 0};
-        lottos.add(lotto);
-        lottoGame.setLottos(lottos);
-        assertThat(lottoGame.calculateRate(result, 1000), is(500.0));
+        lottoGame.setWinningLotto(lotto, 5);
+        assertThat(lottoGame.calculateRate(14000), is((double) 30000000 / 14000 * 100.0));
     }
 
     @Test
     public void 상금계산() {
-        int[] result = new int[]{0, 0, 0, 1, 0, 0, 0};
-        assertThat(lottoGame.calculateReward(result), is(5000));
+        lottoGame.setWinningLotto(lotto, 5);
+        assertThat(lottoGame.calculatIncome(), is(30000000));
     }
 }
