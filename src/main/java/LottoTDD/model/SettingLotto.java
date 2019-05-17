366407c12355f7e459d6b8452740504fc5c957c5
Ilisha <40922963+Ilisha29@users.noreply.github.com>
2019-05-18 06:31:47 +0900

    feat : 로또여러장생성확인 test pass & production code

--- src/main/java/LottoTDD/model/SettingLotto.java
+++ src/main/java/LottoTDD/model/SettingLotto.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SettingLotto {
-    /*public List<Lotto> lottoss;
-    public void settingLotto(int numberOfLotto) {
-        lottoss = new ArrayList<>();
+    public List<Lotto> lottoss;
+    public List<Lotto> settingLotto(int numberOfLotto) {
+        lottoss = new ArrayList<Lotto>();
         for (int i = 0; i <  numberOfLotto; i++) {
             Lotto lotto = new Lotto();
+            lottoss.add(lotto);
         }
-    }*/
+        return lottoss;
+    }
 }
