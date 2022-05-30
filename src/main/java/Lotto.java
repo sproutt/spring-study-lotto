afe634748fb31ef308af5d274064f38d1285f340
mywnajsldkf <mywnajsldkf@gmail.com>
2022-05-31 01:56:08 +0900

    feat : 중복된 로또 번호 검증

--- src/main/java/Lotto.java
+++ src/main/java/Lotto.java
@@ -1,4 +1,6 @@
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     public static final int LOTTO_SIZE = 6;
@@ -6,6 +8,7 @@ public class Lotto {
 
     public Lotto(List<LottoNumber> lotto){
         validateLottoSize(lotto);
+        validateLottoIsUnique(lotto);
         this.lotto = lotto;
     }
 
@@ -13,5 +16,21 @@ public class Lotto {
         if (!(lotto.size() == LOTTO_SIZE)){
             throw new IllegalArgumentException();
         }
+    }
+    
+    private void validateLottoIsUnique(List<LottoNumber> lotto){
+        Set<LottoNumber> lottoNumberSet = new HashSet<>(lotto);
+
+        if (lottoNumberSet.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("중복되는 값이 있습니다.");
+        }
+    }
+
+    public int getLottoSize() {
+        return lotto.size();
+    }
+
+    public List<LottoNumber> getLotto() {
+        return lotto;
     }
 }
