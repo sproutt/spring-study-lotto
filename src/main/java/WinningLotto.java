2cc21be04636d483aa9c0f7eed1c9489db343b27
mywnajsldkf <mywnajsldkf@gmail.com>
2022-05-31 01:58:21 +0900

    feat : 로또 중복 검증

--- src/main/java/WinningLotto.java
+++ src/main/java/WinningLotto.java
@@ -1,4 +1,6 @@
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WinningLotto {
     private List<LottoNumber> winningLotto;
@@ -12,5 +14,17 @@ public class WinningLotto {
         if (!(winningLotto.size() == Lotto.LOTTO_SIZE)){
             throw new IllegalArgumentException();
         }
+    }
+
+    private void validateWinningLottoIsUnique(List<LottoNumber> lotto){
+        Set<LottoNumber> WinningLottoNumberSet = new HashSet<>(lotto);
+
+        if (WinningLottoNumberSet.size() != Lotto.LOTTO_SIZE) {
+            throw new IllegalArgumentException("중복되는 값이 있습니다.");
+        }
+    }
+
+    public int getWinningLottoSize() {
+        return winningLotto.size();
     }
 }
