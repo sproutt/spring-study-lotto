53a95784af2eb742528157f5a052f1195bc1dc47
mywnajsldkf <mywnajsldkf@gmail.com>
2022-05-31 01:57:54 +0900

    test : 성공/실패 테스트 추가
    - [성공] 생성된 로또 번호 개수가 6개인 경우
    - [실패] 중복된 로또 번호가 있는 경우
    - [성공] 생성된 로또 번호가 모두 다른 경우

--- src/test/java/WinningLottoTest.java
+++ src/test/java/WinningLottoTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class WinningLottoTest {
@@ -16,7 +17,50 @@ public class WinningLottoTest {
             testWinningLotto.add(new LottoNumber(i));
         }
 
-        assertThatThrownBy(() -> new WinningLotto(testWinningLotto)).
-                isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new WinningLotto(testWinningLotto))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @Description("로또 개수가 6개라면 성공한다.")
+    public void should_success_when_winning_lotto_size_is_6() {
+        List<LottoNumber> testLotto = new ArrayList<>();
+
+        for (int i = 1; i < 7; i++){
+            testLotto.add(new LottoNumber(i));
+        }
+
+        WinningLotto winningLotto = new WinningLotto(testLotto);
+
+        assertThat(winningLotto.getWinningLottoSize()).isEqualTo(Lotto.LOTTO_SIZE);
+    }
+
+    @Test
+    @Description("로또가 서로 다른 로또 번호로 이루어지지 않으면 예외를 발생한다.")
+    public void should_throw_exception_when_winning_lotto_is_not_unique() {
+        List<LottoNumber> testLotto = new ArrayList<>();
+
+        for (int i = 1; i < 6; i++){
+            testLotto.add(new LottoNumber(i));
+        }
+
+        testLotto.add(new LottoNumber(5));
+
+        assertThatThrownBy(() -> new WinningLotto(testLotto))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @Description("로또가 서로 다른 로또 번호로 이루어지면 성공한다.")
+    public void should_success_when_winning_lotto_is_unique() {
+        List<LottoNumber> testLotto = new ArrayList<>();
+
+        for (int i = 1; i < 7; i++){
+            testLotto.add(new LottoNumber(i));
+        }
+
+        WinningLotto lotto = new WinningLotto(testLotto);
+
+        assertThat(lotto.getWinningLottoSize()).isEqualTo(Lotto.LOTTO_SIZE);
     }
 }
