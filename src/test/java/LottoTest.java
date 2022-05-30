a6ce4bbb354997c4d82a1cef1a59719a2d37ccc6
mywnajsldkf <mywnajsldkf@gmail.com>
2022-05-30 13:59:20 +0900

    test : 로또 크기 테스트 실패

---
+++ src/test/java/LottoTest.java
@@ -0,0 +1,11 @@
+import jdk.jfr.Description;
+import org.junit.Test;
+
+public class LottoTest {
+
+    @Test
+    @Description("로또 개수가 6개인지 확인")
+    public void test_lotto_size_is_6(){
+        Lotto lotto = new Lotto();
+    }
+}
