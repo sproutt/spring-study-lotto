239cf0a698fab231088dd6b60ddd3483650bcfb1
mywnajsldkf <mywnajsldkf@gmail.com>
2022-05-31 01:47:16 +0900

    test : 로또 번호 범위 내에 존재 시 성공 케이스 테스트

--- src/test/java/LottoNumberTest.java
+++ src/test/java/LottoNumberTest.java
@@ -9,7 +9,15 @@ public class LottoNumberTest {
     public void test_lotto_number_range_from_1_to_45(){
         int testNumber = 52;
 
-        assertThatThrownBy(()->new LottoNumber(testNumber)).
-                isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(()->new LottoNumber(testNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @Description("생성된 로또 번호가 범위안에 있으면 성공이다.")
+    public void should_success_when_number_in_range() {
+        int testNumber = 45;
+
+        assertThat(new LottoNumber(testNumber).getNumber()).isEqualTo(testNumber);
     }
 }
