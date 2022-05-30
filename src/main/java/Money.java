15fd1c6868bcefc811aed9648c53d7c0ee70a81e
mywnajsldkf <mywnajsldkf@gmail.com>
2022-05-31 01:52:55 +0900

    refactor : 메서드명 수정
    - [삭제] 돈이 1000원으로 나누어떨어지는지 검증 메서드

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -3,20 +3,13 @@ public class Money {
     private static final int UNIT_MONEY = 1000;
     private final int money;
 
-    public Money(int money){
-        validate_over_1000(money);
-        validate_money_divide_by_1000(money);
+    public Money(int money) {
+        validateOverMIN_MONEY(money);
         this.money = money;
     }
 
-    private void validate_over_1000(int money){
-        if (money < MIN_MONEY){
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private void validate_money_divide_by_1000(int money){
-        if (!(money % 1000 == 0)){
+    private void validateOverMIN_MONEY(int money) {
+        if (money < MIN_MONEY) {
             throw new IllegalArgumentException();
         }
     }
