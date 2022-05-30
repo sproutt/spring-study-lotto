fc1b772d349b2ce580a8fd7bc4a78bdfd9001b66
mywnajsldkf <mywnajsldkf@gmail.com>
2022-05-30 16:47:06 +0900

    test : WinningLottoTest 실패
    - WinningLotto 객체 없음

---
+++ src/test/java/WinningLottoTest.java
@@ -0,0 +1,10 @@
+import jdk.jfr.Description;
+import org.junit.Test;
+
+public class WinningLottoTest {
+    @Test
+    @Description("당첨 로또 번호가 6개인지 확인")
+    public void test_winning_lotto_size_is_6(){
+        WinningLotto winningLotto = new WinningLotto();
+    }
+}
