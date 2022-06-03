d3c8814cbc7e05416174aae1b2aed45c89f6b112
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-03 16:32:59 +0900

    test : 구입금액만큼 로또가 생성되면 성공 케이스

--- src/test/java/LottoTicketTest.java
+++ src/test/java/LottoTicketTest.java
@@ -3,6 +3,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTicketTest {
     @Test
+    @DisplayName("구입금액만큼 로또가 생성된다면 성공이다.")
     public void should_success_when_lotto_ticket_has_lottos_as_much_as_money() {
         // given
         int lottoCount = 14;
