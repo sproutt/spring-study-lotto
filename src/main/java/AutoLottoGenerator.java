4005c6e827ca9f0d3b3e3615662071b9929ea9a2
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-03 18:18:04 +0900

    refactor : 이름 수정(manual -> auto)

--- src/main/java/AutoLottoGenerator.java
+++ src/main/java/AutoLottoGenerator.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class AutoLottoGenerator {
 
     public Lotto generate() {
-
         List<LottoNumber> lotto = new ArrayList<>();
         for(int i = LottoNumber.MIN_NUMBER; i <= LottoNumber.MAX_NUMBER; i++) {
             lotto.add(new LottoNumber(i));
