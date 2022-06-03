9b865723ad2ce41eca029e02b5e59a1b9b400367
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-03 16:44:52 +0900

    refactor : 테스트 메서드 이름 수정

--- src/test/java/LottoTicketTest.java
+++ src/test/java/LottoTicketTest.java
@@ -3,8 +3,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTicketTest {
     @Test
-    @DisplayName("구입금액만큼 로또가 생성된다면 성공이다.")
-    public void should_success_when_lotto_ticket_has_lottos_as_much_as_money() {
+    @DisplayName("구입금액만큼 로또 티켓에 로또가 추가된다면 성공이다.")
+    public void should_success_when_lotto_ticket_can_add_lottos_as_much_as_money() {
         // given
         int lottoCount = 14;
         LottoTicket lottoTicket = new LottoTicket();
