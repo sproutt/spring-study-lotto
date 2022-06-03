b2d24e233cee966ea88b32189f6b41054e60c670
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-03 18:17:49 +0900

    refactor : 이름 수정(manual -> auto)

--- src/test/java/AutoLottoGeneratorTest.java
+++ src/test/java/AutoLottoGeneratorTest.java
@@ -3,16 +3,16 @@ import org.junit.jupiter.api.DisplayName;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ManualLottoGeneratorTest {
+public class AutoLottoGeneratorTest {
 
     @Test
     @DisplayName("수동 로또 제너레이터가 로또를 생성한다.")
     public void should_success_when_manual_lotto_generator_generate_lotto() {
         // given
-        AutoLottoGenerator manualLottoGenerator = new AutoLottoGenerator();
+        AutoLottoGenerator autoLottoGenerator = new AutoLottoGenerator();
 
         // when
-        Lotto lotto = manualLottoGenerator.generate();
+        Lotto lotto = autoLottoGenerator.generate();
 
         // then
         assertThat(lotto).isNotNull();
--- src/test/java/LottoTicketTest.java
+++ src/test/java/LottoTicketTest.java
@@ -1,15 +1,26 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.ArrayList;
+import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LottoTicketTest {
+
     @Test
     @DisplayName("구입금액만큼 로또 티켓에 로또가 추가된다면 성공이다.")
     public void should_success_when_lotto_ticket_can_add_lottos_as_much_as_money() {
         // given
-        int lottoCount = 14;
-        LottoTicket lottoTicket = new LottoTicket();
+        int lottoCount = 3;
+        List<Lotto> lottos = new ArrayList<>();
+        LottoTicket lottoTicket = new LottoTicket(lottos);
+        AutoLottoGenerator autoLottoGenerator = new AutoLottoGenerator();
+
         // when
-        
+        for(int i = 0; i < lottoCount; i++) {
+            lottoTicket.add(autoLottoGenerator.generate());
+        }
+
         // then
         assertThat(lottoTicket.getLottoCount()).isEqualTo(lottoCount);
     }
