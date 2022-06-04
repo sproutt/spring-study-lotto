50e5d51bc4618ceb33052c3560ede67d48d754a0
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-04 20:42:37 +0900

    test : 실제 로또 수익률과 예상한 로또 수익률이 같은지 테스트

--- src/test/java/LottoGameTest.java
+++ src/test/java/LottoGameTest.java
@@ -9,10 +9,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoGameTest {
     @Test
     @DisplayName("실제 로또 수익률과 예상한 로또 수익률이 같다면 성공이다.")
-    public void should_success_when_real_lotto_yield_matches_expected_lotto_yield(){
+    public void should_success_when_real_lotto_earning_ratio_matches_expected_lotto_yield() {
         // given
-        double expectedLottoYield = 35.7;
-
+        LottoGame lottoGame = new LottoGame();
         Map<Rank, Integer> statistics = new HashMap<>();
         for (Rank value : Rank.values()){
             statistics.put(value, 0);
@@ -21,6 +20,6 @@ public class LottoGameTest {
         // when
 
         // then
-        assertThat(realLottoYield).isEqualTo(expectedLottoYield);
+        assertThat(earningRatio).isEqualTo(expectedEarningRatio);
     }
 }
