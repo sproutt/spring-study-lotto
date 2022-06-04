513d4b75f0937385a26d7e8174c46b0faa8dc968
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-04 17:51:15 +0900

    test : LottoGameTest 실패 케이스 생성

---
+++ src/test/java/LottoGameTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoGameTest {
+    @Test
+    @DisplayName("실제 로또 수익률과 예상한 로또 수익률이 같다면 성공이다.")
+    public void should_success_when_real_lotto_yield_matches_expected_lotto_yield(){
+        // given
+        double expectedLottoYield = 35.7;
+
+        Map<Rank, Integer> statistics = new HashMap<>();
+        for (Rank value : Rank.values()){
+            statistics.put(value, 0);
+        }
+        statistics.put(Rank.FOURTH, 1);
+        // when
+
+        // then
+        assertThat(realLottoYield).isEqualTo(expectedLottoYield);
+    }
+}
