9ce1203ca3190bee5ee6e277351c42978ab59f87
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-19 10:55:57 +0900

    refactor : List<LottoNumber> 인스턴스 선언 시점과 초기화 시점 수정

--- src/main/java/AutoLottoGenerator.java
+++ src/main/java/AutoLottoGenerator.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 public class AutoLottoGenerator {
 
-    public static Lotto generate() {
-        List<LottoNumber> lotto = new ArrayList<>();
+    private static List<LottoNumber> lotto;
+
+    static {
+        lotto = new ArrayList<>();
         for(int i = LottoNumber.MIN_NUMBER; i <= LottoNumber.MAX_NUMBER; i++) {
             lotto.add(LottoNumber.of(i));
         }
+    }
+
+    public static Lotto generate() {
         Collections.shuffle(lotto);
         return new Lotto(lotto.subList(0,6));
     }
--- src/test/java/AutoLottoGeneratorTest.java
+++ src/test/java/AutoLottoGeneratorTest.java
@@ -1,5 +1,5 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
