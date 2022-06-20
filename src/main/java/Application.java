7684773807236e8cc5af0994988437db268dece4
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-20 22:23:12 +0900

    feature : 구입 금액 Money로 포장

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,7 @@
 public class Application {
     public static void main(String[] args) {
         LottoGame game = new LottoGame();
-        OutputView.printLottoCount(game.inputMoney(InputView.inputMoney()));
+        OutputView.printLottoCount(game.inputMoney(new Money(InputView.inputMoney()).getMoney()));
         OutputView.printLottoTicket(game.generateLottoTicket());
         WinningLotto winningLotto = new WinningLotto(StringParsingUtils.parseToLottoNumber(InputView.inputWinningLottoNumber()));
         OutputView.printStatistics(game.calculateRankStatistics(winningLotto));
