3223961685c843e744d588fbe9e19a5aa691a51d
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-21 21:44:34 +0900

    style : 코드 리포맷핑

--- src/test/java/LottoNumberTest.java
+++ src/test/java/LottoNumberTest.java
@@ -1,6 +1,6 @@
 import exception.LottoNumberOutOfRangeException;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
