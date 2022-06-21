e60b04113dc219aa0747c740c1b46f4c4a8e6948
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-21 21:32:53 +0900

    refactor : Lotto를 사용하여 조립하는 방식으로 변경

--- src/main/java/WinningLotto.java
+++ src/main/java/WinningLotto.java
@@ -6,35 +6,17 @@ import java.util.List;
 import java.util.Set;
 
 public class WinningLotto {
-    private static final String WINNING_LOTTO_SIZE = "당첨 로또 크기가 6이 아닙니다.";
-    private static final String LOTTO_IS_UNIQUE = "중복된 당첨 번호가 있습니다";
-    private List<LottoNumber> winningLotto;
+    private Lotto lotto;
 
     public WinningLotto(List<LottoNumber> winningLotto) {
-        validateWinningLottoSize(winningLotto);
-        validateWinningLottoIsUnique(winningLotto);
-        this.winningLotto = winningLotto;
-    }
-
-    private void validateWinningLottoSize(List<LottoNumber> winningLotto) {
-        if (!(winningLotto.size() == Lotto.LOTTO_SIZE)) {
-            throw new LottoSizeMismatchException(WINNING_LOTTO_SIZE);
-        }
-    }
-
-    private void validateWinningLottoIsUnique(List<LottoNumber> lotto) {
-        Set<LottoNumber> WinningLottoNumberSet = new HashSet<>(lotto);
-
-        if (WinningLottoNumberSet.size() != Lotto.LOTTO_SIZE) {
-            throw new LottoIsNotUniqueException(LOTTO_IS_UNIQUE);
-        }
+        this.lotto = new Lotto(winningLotto);
     }
 
     public int getWinningLottoSize() {
-        return winningLotto.size();
+        return lotto.getLottoSize();
     }
 
     public boolean contains(LottoNumber lottoNumber) {
-        return winningLotto.contains(lottoNumber);
+        return lotto.contains(lottoNumber);
     }
 }
