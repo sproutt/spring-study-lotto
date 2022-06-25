414bdabacca8fc66322e5b7bbf1c4ad40326b716
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-25 16:56:21 +0900

    feat : BonusMatchCount 파라미터 추가

--- src/main/java/Rank.java
+++ src/main/java/Rank.java
@@ -1,17 +1,20 @@
 import java.util.Arrays;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FOURTH(3, 5000),
-    NO_RANK(0, 0);
+    FIRST(6, 0,2000000000),
+    SECOND(5, 1,1500000),
+    THIRD(4, 0,50000),
+    FOURTH(3, 0,5000),
+    NO_RANK(0, 0,0);
 
     private final int matchCount;
+    private final int bonusMatchCount;
     private final int reward;
 
-    Rank(int matchCount, int reward) {
+
+    Rank(int matchCount, int bonusMatchCount, int reward) {
         this.matchCount = matchCount;
+        this.bonusMatchCount = bonusMatchCount;
         this.reward = reward;
     }
 
@@ -19,13 +22,17 @@ public enum Rank {
         return matchCount;
     }
 
+    public int getBonusMatchCount() {
+        return bonusMatchCount;
+    }
+
     public int getReward() {
         return reward;
     }
 
-    public static Rank of(int matchCount) {
+    public static Rank of(int matchCount, int bonusMatchCount) {
         return Arrays.stream(Rank.values())
-                .filter(rank -> rank.matchCount == matchCount)
+                .filter(rank -> rank.matchCount == matchCount && rank.bonusMatchCount == bonusMatchCount)
                 .findFirst()
                 .orElse(NO_RANK);
     }
