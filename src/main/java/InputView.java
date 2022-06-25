0a100d3dd5c510dd67375d8c70618f8f626b8506
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-25 15:25:05 +0900

    feat: 보너스 볼 입력 기능 추가

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -9,6 +9,8 @@ public class InputView {
     private static final String MANUAL_LOTTO_COUNT_MESSAGE = "수동으로 구매할 로또 수를 입력해 주세요.";
     private static final String MANUAL_LOTTO_NUMBER_MESSAGE = "수동으로 구매할 번호를 입력해 주세요.";
 
+    private static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 볼을 입력해 주세요.";
+
     private static Scanner scanner = new Scanner(System.in);
 
     public static int inputMoney() {
@@ -16,7 +18,7 @@ public class InputView {
         return Integer.parseInt(scanner.nextLine());
     }
 
-    public static int manualLottoCount(){
+    public static int manualLottoCount() {
         System.out.println(MANUAL_LOTTO_COUNT_MESSAGE);
         return Integer.parseInt(scanner.nextLine());
     }
@@ -32,6 +34,11 @@ public class InputView {
             manualLotto.add(lottoNumbers);
         }
         return manualLotto;
+    }
+
+    public static int inputBonusNumber() {
+        System.out.println(INPUT_BONUS_NUMBER_MESSAGE);
+        return Integer.parseInt(scanner.nextLine());
     }
 
     public static String inputWinningLottoNumber() {
