4f64097ed5eccb240ec5330b37ea1b5931cf0252
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-25 17:47:33 +0900

    test : 2등 당첨 확인 테스트 구현

--- src/test/java/LottoTicketTest.java
+++ src/test/java/LottoTicketTest.java
@@ -30,4 +30,32 @@ public class LottoTicketTest {
         // then
         assertThat(statistics.get(Rank.FIRST)).isEqualTo(1);
     }
+
+    @Test
+    @DisplayName("당첨 번호와 일치하는 개수가 5개이고, 일치하는 보너스 번호 개수가 1일때 2등의 개수가 1이면 성공이다.")
+    public void should_success_when_winning_lotto_count_is_5_and_bonus_number_is_1_return_2nd_count() {
+        // given
+        List<LottoNumber> winningLottoNumbers = new ArrayList<>();
+        for(int i = 1; i <= Lotto.LOTTO_SIZE; i ++) {
+            winningLottoNumbers.add(new LottoNumber(i));
+        }
+
+        List<LottoNumber> testLottoNumber = new ArrayList<>();
+        for (int i=2; i <= Lotto.LOTTO_SIZE + 1; i++) {
+            testLottoNumber.add(new LottoNumber(i));
+        }
+        Lotto secondLotto = new Lotto(testLottoNumber);
+        List<Lotto> lottos = new ArrayList<>();
+        lottos.add(secondLotto);
+
+        LottoTicket lottoTicket = new LottoTicket(lottos);
+
+        WinningLotto winningLotto = new WinningLotto(winningLottoNumbers, new BonusNumber(new LottoNumber(7)));
+
+        // when
+        Map<Rank, Integer> statistics = lottoTicket.compareWithWinningLotto(new TreeMap<>(), winningLotto);
+
+        // then
+        assertThat(statistics.get(Rank.SECOND)).isEqualTo(1);
+    }
 }
