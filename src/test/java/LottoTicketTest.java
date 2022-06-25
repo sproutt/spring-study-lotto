7aafc7428a5dbc10a079323a415b1aafbdece3c3
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-25 16:16:07 +0900

    refactor: 테스트 위치 변경

--- src/test/java/LottoTicketTest.java
+++ src/test/java/LottoTicketTest.java
@@ -11,24 +11,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LottoTicketTest {
 
     @Test
-    @DisplayName("구입 장수만큼 로또 티켓에 로또가 추가된다면 성공이다.")
-    public void should_success_when_lotto_ticket_can_add_lottos_as_much_as_money() {
-        // given
-        int lottoCount = 3;
-        List<Lotto> lottos = new ArrayList<>();
-        LottoTicket lottoTicket = new LottoTicket(lottos);
-        AutoLottoGenerator autoLottoGenerator = new AutoLottoGenerator();
-
-        // when
-        for(int i = 0; i < lottoCount; i++) {
-            lottoTicket.add(autoLottoGenerator.generate());
-        }
-
-        // then
-        assertThat(lottoTicket.getLottoCount()).isEqualTo(lottoCount);
-    }
-
-    @Test
     @DisplayName("당첨 번호를 넣었을 때, 로또 티켓에서 1등을 1장 반환하면 성공한다.")
     public void should_success_when_input_winning_lotto_number_return_one_first_prize_in_lotto_ticket() {
         // given
