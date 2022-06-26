efe55de545fece5664b3b83c6044a3db95b0f726
mywnajsldkf <mywnajsldkf@gmail.com>
2022-06-26 14:13:04 +0900

    feat: 입력값 예외처리

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,16 +1,33 @@
+import exception.*;
+
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         LottoGame game = new LottoGame();
-        OutputView.printLottoCount(game.inputMoney(new Money(InputView.inputMoney()).getMoney()));
-        int lottoCount = InputView.manualLottoCount();
-        List<Lotto> lottos = InputView.manualLotto(lottoCount);
-        OutputView.printLottoTicket(game.generateLottoTicket(lottos));
-        List<LottoNumber> lottoNumbers = StringParsingUtils.parseToLottoNumber(InputView.inputWinningLottoNumber());
-        BonusNumber bonusNumber = InputView.inputBonusNumber();
-        WinningLotto winningLotto = new WinningLotto(lottoNumbers, bonusNumber);
-        OutputView.printStatistics(game.calculateRankStatistics(winningLotto));
-        OutputView.printEarningRate(game.calculateEarningRatio());
+        while (true) {
+            try {
+                OutputView.printLottoCount(game.inputMoney(new Money(InputView.inputMoney()).getMoney()));
+                int lottoCount = InputView.manualLottoCount();
+                List<Lotto> lottos = InputView.manualLotto(lottoCount);
+                OutputView.printLottoTicket(game.generateLottoTicket(lottos));
+                List<LottoNumber> lottoNumbers = StringParsingUtils.parseToLottoNumber(InputView.inputWinningLottoNumber());
+                BonusNumber bonusNumber = InputView.inputBonusNumber();
+                WinningLotto winningLotto = new WinningLotto(lottoNumbers, bonusNumber);
+                OutputView.printStatistics(game.calculateRankStatistics(winningLotto));
+                OutputView.printEarningRate(game.calculateEarningRatio());
+                break;
+            } catch (MoneyUnderMinMoneyException moneyUnderMinMoneyException) {
+                System.out.println(moneyUnderMinMoneyException.getMessage());
+            } catch (LottoSizeMismatchException lottoSizeMismatchException) {
+                System.out.println(lottoSizeMismatchException.getMessage());
+            } catch (LottoIsNotUniqueException lottoIsNotUniqueException) {
+                System.out.println(lottoIsNotUniqueException.getMessage());
+            } catch (LottoNumberOutOfRangeException lottoNumberOutOfRangeException) {
+                System.out.println(lottoNumberOutOfRangeException.getMessage());
+            } catch (BonusNumberDuplicatedException bonusNumberDuplicatedException) {
+                System.out.println(bonusNumberDuplicatedException.getMessage());
+            }
+        }
     }
 }
